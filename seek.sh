f07271f173a90ebcf8140e60a34d3eb56027d3c5
Matt Wagner <matt@wags.us>
2016-04-15 21:19:02 -0400

    Set up getopts and custom error handling
    Set up a function for custom error handling, and use getopts to build
    the basic structure to parse arguments passed from the command line.
    
    RE: #5

--- seek.sh
+++ seek.sh
@@ -11,6 +11,41 @@ Find top repositories for a given query
 END
 }
 
+# Function for custom error handling
+# 1st argument: error message to print
+# 2nd argument: exit code to exit script with
+# Finally, redirect to stderr
+error () {
+    echo "Error: $1"
+    usage
+    exit $2
+} >&2
+
+while getopts ":s:l:t:" opt; do
+    case $opt in
+        s)
+            echo "-s detected with argument: ${OPTARG}"
+            ;;
+        l)
+            echo "-l detected with argument: ${OPTARG}"
+            ;;
+        t)
+            echo "-t detected with argument: ${OPTARG}"
+            ;;
+        :)
+            error "Option -${OPTARG} is missing an argument" 2
+            ;;
+        \?)
+            error "Unknown option: -${OPTARG}" 3
+            ;;
+    esac
+done
+
+# Gets the index of the next argument that getopts was going to handle,
+# then uses that as the value to pass to shift.
+shift $(( OPTIND -1 ))
+echo "remaining input: $1"
+
 set_vars () {
     AUTH_STRING="Authorization: token ${GITHUB_ACCESS_TOKEN}"
 }
@@ -19,7 +54,7 @@ get_data () {
     URL="https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc"
     response_data=$(curl -s -H \
         "${AUTH_STRING}" "${URL}")
-    echo $response_data | jq ".items[0,1,2,3,4] | {name, description, stargazers_count}"
+    echo "$response_data" | jq ".items[0,1,2,3,4] | {name, description, stargazers_count, forks_count, updated_at}"
 }
 
 set_vars
